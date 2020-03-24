ac732ce531ba774d2df2583674118c6ac295376d
Juan Restrepo <juan.restrepop@gmail.com>
2020-03-24 13:00:39 +0100

    [Button] Add button component

---
+++ src/components/Button/Button.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Button from '.';
+
+describe('Button component', () => {
+    it('should be defined', () => {
+        expect(Button).toBeDefined();
+    });
+})
---
+++ src/components/Button/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import  PropTypes from 'prop-types';
+
+const BUTTON_ID = 'button-component';
+
+function _generateButtonClassName(type) {
+    if (type === 'success') {
+        return `${BUTTON_ID} ${BUTTON_ID}--success`;
+    } else if( type === 'danger') {
+        return `${BUTTON_ID} ${BUTTON_ID}--danger`;
+    }
+}
+
+function Button(props) {
+    const {
+        title, type,
+        onClick
+    } = props;
+
+    return (
+        <button className={_generateButtonClassName(type)} data-testid={BUTTON_ID}
+            onClick={onClick}
+        >
+            { title }
+        </button>
+    );
+}
+
+Button.propTypes = {
+    title: PropTypes.string,
+    type: PropTypes.oneOf(['success', 'danger']),
+    onClick: PropTypes.func.isRequired,
+}
+
+Button.defaultProps = {
+    type: 'success',
+}
+
+export default Button;
