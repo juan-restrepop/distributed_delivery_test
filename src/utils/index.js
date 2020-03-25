9512f955b550645f72a62c5a01293c55b9010eb0
Juan Restrepo <juan.restrepop@gmail.com>
2020-03-25 15:44:26 +0100

    [SessionToken] Store session token in `window.sessionStorage`

---
+++ src/utils/index.js
@@ -0,0 +1,17 @@
+function storeToken(token) {
+    return sessionStorage.setItem('session_token', token);
+};
+
+function getToken() {
+    return sessionStorage.getItem('session_token');
+};
+
+function clearToken() {
+    return sessionStorage.removeItem('session_token');
+};
+
+export {
+    storeToken,
+    getToken,
+    clearToken,
+}
