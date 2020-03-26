995573d6af394441b8af0174b4dcc66ca8748d3d
Juan Restrepo <juan.restrepop@gmail.com>
2020-03-26 15:15:06 +0100

    [Routing] Setup routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import React from 'react';
 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
 // Import components
 import Home from './containers/Home';
 import LogIn from './containers/LogIn';
 import Dashboard from './containers/Dashboard';
 
+const routes = [
+    {
+        path: '/login',
+        component: LogIn,
+        id:'login-route',
+    },
+    {
+        path: '/dashboard',
+        component: Dashboard,
+        id:'dashboard-route',
+    },
+    {
+        path: '/',
+        component: Home,
+        id:'home-route',
+    },
+]
+
 // Main container
 function App() {
-  return (
-      <div>
-          <Home />
-          <LogIn />
-          <Dashboard />
-      </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Switch>
+                    {routes.map(
+                        route => <Route path={route.path} component={route.component} id/>
+                    )}
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
