159ef2db60701faf3cdfb3b0697a44aa9ea0b01d
Juan Restrepo <juan.restrepop@gmail.com>
2020-03-26 18:31:40 +0100

    [LogIn] Adapt container for protected routes
    * If the client requests a protect view she will be redirected after a 
    succesful login

--- src/containers/LogIn/index.js
+++ src/containers/LogIn/index.js
@@ -1,21 +1,43 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import LogInPresentational from './LogInPresentational.js';
 
 import { requestLogIn } from '../../api/';
 
-function _onLogIn(id, pwd) {
-    const requestParameters = {
-        identifiant: id,
-        password: pwd
-    };
+function LogIn(props) {
+    function _onLogIn(id, pwd) {
+        const requestParameters = {
+            identifiant: id,
+            password: pwd
+        };
 
-    return requestLogIn(requestParameters);
-}
+        let referrer, location;
+        if (!props.location.state) {
+            referrer = false;
+            location = '';
+        } else {
+            referrer = props.location.state.referrer;
+            location = props.location.state.fromLocation;
+        }
 
-function LogIn() {
+        return requestLogIn(requestParameters)
+            .then(data => {
+                debugger
+                if (!referrer) {
+                    debugger
+                    return props.history.push('/')
+                } else {
+                    debugger
+                    return props.history.push(location);
+                }
+            })
+    }
+
+    debugger
+
     return (
-        <LogInPresentational onLogIn={_onLogIn}/>
+        <LogInPresentational onLogIn={_onLogIn} {...props}/>
     )
 }
 
